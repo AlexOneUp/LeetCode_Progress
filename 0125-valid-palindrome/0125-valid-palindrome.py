b5893b5e1fca18fa379f7dde47be309767b0c0d2
Alex <ruanalex11235@gmail.com>
2023-01-12 00:36:00 -0500

    Time: 41 ms (94.59%), Space: 14.7 MB (43.13%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,21 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-        String lowerCased = s.toLowerCase();
-        String noSpecialChar = lowerCased.replaceAll("([^a-z0-9])", "");
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
         
-        int stringLength = noSpecialChar.length();
+        l = 0
+        r = len(s)-1
         
-        //Two Pointer Approach
-        int j = stringLength - 1;
-        int i = 0;
-        while (i < j) {
-            if(noSpecialChar.charAt(i) != noSpecialChar.charAt(j) ) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
-}
-
-//Two Pointer Approach :
-//___________________________________________________________________
-// lowercase input, then regex lowercased input, then 2 ptr approach for each char in regex replaced string. If unmatching chars, then return false.
+        while l < r :
+            
+            if s[l].isalnum() == False:
+                l+=1
+                
+            elif s[r].isalnum() == False:
+                r-=1
+                
+            elif s[l] != s[r]:
+                return False
+            else:
+                l+=1
+                r-=1
+        return True

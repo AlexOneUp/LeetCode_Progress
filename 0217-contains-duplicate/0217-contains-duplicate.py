2f1af4d7a79dd1e9408192f1945e2b619962bb37
Alex <ruanalex11235@gmail.com>
2023-01-12 00:48:08 -0500

    Time: 447 ms (95.41%), Space: 25.9 MB (91.76%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,9 +4,9 @@ class Solution:
         hashmap = {}
         for num in nums:
             hashmap[num] = hashmap.get(num,0) + 1
+            
             if hashmap[num] >= 2:
                return True
-            else:
-                continue
+
         return False    
                 

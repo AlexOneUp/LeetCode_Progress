fc3a5374160acc645ed200dcbf4adff76a6be51f
Alex <ruanalex11235@gmail.com>
2023-01-12 15:00:03 -0500

    Time: 115 ms (67.16%), Space: 19 MB (35.80%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,30 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        hashmap = {}
+#         hashmap = {}
 
-        for subStr in strs:
-            sortWord = ''.join(sorted(subStr))
+#         for subStr in strs:
+#             sortWord = ''.join(sorted(subStr))
 
-            if sortWord not in hashmap:
-                hashmap[sortWord] = [subStr]
+#             if sortWord not in hashmap:
+#                 hashmap[sortWord] = [subStr]
+#             else:
+#                 hashmap[sortWord] += [subStr]
+#         return list(hashmap.values())
+
+# Create an empty dictionary to store the anagrams
+        anagrams = {}
+        # Iterate through the list of strings
+        for s in strs:
+            # Count the number of each character in the string
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            # Use the count as a key to store the original string in the dictionary
+            count = tuple(count)
+            if count in anagrams:
+                anagrams[count].append(s)
             else:
-                hashmap[sortWord] += [subStr]
-        return list(hashmap.values())
+                anagrams[count] = [s]
+        # Return the values of the dictionary
+        return list(anagrams.values())

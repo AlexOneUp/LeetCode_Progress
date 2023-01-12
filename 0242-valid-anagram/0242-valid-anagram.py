70a33ee03e27aa9d122fdc206b227faad99aaeb7
Alex <ruanalex11235@gmail.com>
2023-01-11 22:43:18 -0500

    Time: 49 ms (85.43%), Space: 14.5 MB (34.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,16 +8,17 @@ class Solution:
             # 1 + hashmap.getOrDefault(key, defaultValue) Python equivalent
             hashmap[char] = hashmap.get(char, 0) + 1
 
-        print(hashmap)
+ 
         for char in t:
             if char in hashmap:
                 hashmap[char] -= 1
             else:
                 return False
         # Check if there are any remaining contents in the hashmap. If there is, then it isn't an anagram
-        # if len(hashmap) != 0:
-        #     return False
+        
+        # Check for each key, to see if it's values are now empty
         for val in hashmap.values():
+            print(val)
             if val != 0:
                 return False
 

beb8536cde8126365a73db12e41d41dd32209269
Alex <ruanalex11235@gmail.com>
2023-01-21 13:22:05 -0500

    Time: 38 ms (70.48%), Space: 14 MB (13.58%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -2,19 +2,22 @@ class Solution:
     def isHappy(self, n: int) -> bool:
 
         while n != 1: 
+            """
+        u can do the summation of x  ^ 2 for each char at string of n
+        sum(iterable, start)
+            """ 
             sq_sum = sum([int(x) ** 2 for x in str(n)])
             # Magic Math property https://en.wikipedia.org/wiki/Happy_number
             if n == 4 :
                 return False
             n = sq_sum
             print(sq_sum)
-            
-        # u can do the summation of x  ^ 2 for each char at string of n
-        # sum(iterable, start)
-        if len(str(n)) == 1 and n != 1:
-            return False
+         
         if len(str(n)) == 1 and n == 1:
             return True
+   
+
+
 
 """
 

88909641a817862699443306b89384c097913c44
Alex <ruanalex11235@gmail.com>
2023-01-26 19:32:27 -0500

    Time: 284 ms (92.27%), Space: 16.3 MB (81.07%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,49 +1,23 @@
-class Solution {
-    public int numIslands(char[][] grid) {
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rowEnd = len(grid) - 1
+        colEnd = len(grid[0]) - 1
         
-        //Set of boundaries in the matrix
-        int rowEnd = grid.length - 1;
-        int colEnd = grid[0].length - 1;
+        islands = 0
         
-        int numberIslands = 0;
-        //This 
-        for( int i = 0; i < grid.length; i++ ) {
-            for(int j = 0; j < grid[0].length; j++){ 
-                if( grid[i][j] == '1' ) {
-                    isIsland(grid, i, j, rowEnd, colEnd);
-                    numberIslands++;
-                }
-            }
-            
-        }
-        return numberIslands;
-    }
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid,i,j,rowEnd,colEnd)
+                    islands += 1
+        return islands
     
-    public void isIsland( char[][] grid, int i, int j, int rowEnd, int colEnd ) {
-        //Boundary check 
-        if( i >= 0 && i <= rowEnd && j >= 0 && j <= colEnd) { 
-            if( grid[i][j] == '1' ) {
-            
-                grid[i][j] = '2';
-                //DFS on piece of land
-                isIsland( grid, i+1 , j , rowEnd, colEnd);
-                isIsland( grid, i-1 , j , rowEnd, colEnd);
-                isIsland( grid, i , j+1 , rowEnd, colEnd);
-                isIsland( grid, i , j-1 , rowEnd, colEnd);
-            }
-        }   
-    }
-}
-
-/**
-Recursive DFS Approach :
-____________________________
-- Look through matrix and for each element, when we find land do DFS.
-- Do a recursive DFS approach helper method to check whether or not there is an island with connected 1s
-    + Do the recursion while in bound of the matrix.
-- If it is an island, redefine the index at the V[i][j] (V =  vertex).
-
-
-Time : O(m * n) for the matrix
-Space : O(m * n) for the DFS CALL STACK
-**/
+    def dfs(self,grid, i, j, rowEnd, colEnd):
+        if (i >= 0 and i <= rowEnd) and (j >= 0 and j <= colEnd):
+            if grid[i][j] == '1':
+                grid[i][j] = '2'
+                
+                self.dfs(grid, i+1, j, rowEnd, colEnd)
+                self.dfs(grid, i-1, j, rowEnd, colEnd)
+                self.dfs(grid, i, j+1, rowEnd, colEnd)
+                self.dfs(grid, i, j-1, rowEnd, colEnd)

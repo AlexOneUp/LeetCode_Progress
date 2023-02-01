c4aaa1eafd00b80f542ffb2168d55772deb542be
Alex <ruanalex11235@gmail.com>
2023-02-01 14:34:00 -0500

    Time: 54 ms (76.17%), Space: 15.2 MB (74.59%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -16,3 +16,18 @@ class Solution:
                 break
 
         return res
+    
+    """
+    Approach :
+        HashMap of all ASCII characters + digits with default value = 0
+        +1 to the key for every time it appears in the string
+        
+        Main idea is the sorted() function to sort the hash map. and reverse the sort so the most frequent are up top
+        then just remake it from a list back into a dictionary
+        
+        Python specific k, v loop through now sorted hashmap
+            if the value is > 0 
+                append k, v times to res
+            break the loop if this isn't true anymore.
+        return res
+    """

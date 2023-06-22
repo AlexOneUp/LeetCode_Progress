c36fac306953a329440a1e0c54ae1a13f99c9168
Alex <ruanalex11235@gmail.com>
2023-06-21 20:56:05 -0400

    Time: 55 ms (45.92%), Space: 16.6 MB (14.44%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,22 +1,20 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-#         hmap = dict()
+        hmap = dict()
         
-#         for n in arr:
-#             if n not in hmap:
-#                 hmap[n] = 1
-#             else :
-#                 hmap[n] += 1   
-# #         Pointer to last value
+        for n in arr:
+            if n not in hmap:
+                hmap[n] = 1
+            else :
+                hmap[n] += 1   
+#         Pointer to last value
 
-#         hmap2 = dict()
-#         for k, v in hmap.items():
-#             if v in hmap2:
-#                 return False
-#             else:
-#                 hmap2[v] = 1
-#         return True
+        hmap2 = dict()
+        for k, v in hmap.items():
+            if v in hmap2:
+                return False
+            else:
+                hmap2[v] = 1
+        return True
         # print(hmap)
-        return all(map(lambda fre: fre == 1, Counter(Counter(arr).values()).values()))
-        
-        
+       

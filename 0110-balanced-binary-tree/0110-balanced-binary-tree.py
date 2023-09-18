b648a4b9e92a2b245387306bcbcc4aecdcc569d1
Alex <ruanalex11235@gmail.com>
2023-09-17 20:29:14 -0400

    Time: 59 ms (37.33%), Space: 21.3 MB (32.36%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -9,22 +9,17 @@ class Solution:
         if not root:
             return True
         
-        res = True
         def dfs(node):
-            nonlocal res
+
+            
             if not node:
-                return 0
+                return [True, 0]
             left = dfs(node.left)
             right = dfs(node.right)    
             
-            if abs(left - right) > 1: 
-                res = False
-            return max(left, right) + 1
-        
-        dfs(root) 
-        
-        return res
-        
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+            return [balanced, 1 + max(left[1], right[1])]     
+        return dfs(root)[0]
         
 
             

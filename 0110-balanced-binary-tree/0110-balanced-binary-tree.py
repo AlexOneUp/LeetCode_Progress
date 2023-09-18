3636b02964b955b2fe856b59d82516edbb38c09f
Alex <ruanalex11235@gmail.com>
2023-09-17 20:57:08 -0400

    Time: 73 ms (7.26%), Space: 21.1 MB (46.79%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -11,11 +11,12 @@ class Solution:
         
         def dfs(node):
 
-            
             if not node:
                 return [True, 0]
             left = dfs(node.left)
+            print(left, "left")
             right = dfs(node.right)    
+            print(right, 'right')
             
             balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
             return [balanced, 1 + max(left[1], right[1])]     

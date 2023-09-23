a752e297a16ce8ef9e26a206e70e707f2d2b0a9f
Alex <ruanalex11235@gmail.com>
2023-09-22 22:01:39 -0400

    Time: 84 ms (34.91%), Space: 26 MB (56.87%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+#         find midpoint list
+        
+        fast, slow = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+#         Now slow is the mid-point   
+        
+        def reverse(node):
+            cur = node
+            prev = None
+        
+            while cur:            
+                nxt = cur.next  
+                cur.next = prev
+                prev = cur
+                cur = nxt
+            return prev
+        
+        midpt = reverse(slow)
+        left = head
+
+        while midpt.next:     
+            next_hop = left.next
+            left.next = midpt
+            left = next_hop
+            
+            next_hop = midpt.next
+            midpt.next = left
+            midpt = next_hop
+            
+# Initial Approach :
+# // Find the midpoint splitting the list into 2 sections w/ 2 pointer approach
+# // Reverse the 2nd half
+# // uses 2 ptr approach to find mid point of LL, reverses the start of the 2nd half, uses other temp nodes to reassign the ptrs.

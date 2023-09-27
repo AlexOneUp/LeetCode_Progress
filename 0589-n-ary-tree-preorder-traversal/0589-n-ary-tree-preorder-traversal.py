f5672266b7501b678a1bd6830b64ea60c5061c80
Alex <ruanalex11235@gmail.com>
2023-09-27 17:28:04 -0400

    Time: 54 ms (48.68%), Space: 18.5 MB (67.88%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+
+        if root == None:
+            return []
+        
+        preorderPath = [] # store preorder path 1
+
+        # check for child
+        # recursively call those steps
+        # How would you do this?
+        def dfs(node):
+            preorderPath.append(node.val)
+            for child in node.children:
+                    dfs(child)
+        dfs(root)
+        return preorderPath
+        return res
+        

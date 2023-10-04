ea85153879603f31d8f4c3c9c18ebc82861075cb
Alex <ruanalex11235@gmail.com>
2023-10-04 13:22:28 -0400

    Time: 208 ms (43.34%), Space: 21.6 MB (35.82%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -1,6 +1,9 @@
 class Solution:
 
     def __init__(self, w: List[int]):
+        # Prefix sums bascially says :
+        # Input : [1,2,4] = 7 (total weight) and there are 1/7, 2/7, 4/7 chances to pick each index.
+        # However, the probability is a range in length of input.
         self.prefix_sums = []
         
         total = 0

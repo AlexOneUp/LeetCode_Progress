ac7e1d8348d5503a5ee486e118fd9a15d8bea71b
Alex <ruanalex11235@gmail.com>
2023-10-15 22:48:17 -0400

    Time: 203 ms (76.98%), Space: 17.7 MB (13.05%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -7,9 +7,7 @@ class Solution:
         cities_in_row = len(isConnected)
         # Represents the columns
        
-        visited = { 
-            city : False for city in range(cities_in_row)
-        }  #{ 0: False, 1: False, 2: False }
+        visited = { city : False for city in range(cities_in_row) }  #{ 0: False, 1: False, 2: False }
     
         # Iter through the rows and see if there is a connecting city.
         def dfs(row):

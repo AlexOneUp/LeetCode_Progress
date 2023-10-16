f25117b28ba6cef04cd90d619251388915634f8a
Alex <ruanalex11235@gmail.com>
2023-10-15 22:44:54 -0400

    Time: 204 ms (73.35%), Space: 17.4 MB (31.87%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,69 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        
+        total_provinces = 0
+        
+        
+        cities_in_row = len(isConnected)
+        # Represents the columns
+        visited = [False] * len(isConnected)
+        
+        # Iter through the rows and see if there is a connecting city.
+        def dfs(row):
+            for col in range(cities_in_row):
+                if isConnected[row][col] and not visited[col]:
+                    visited[col] = True
+                    dfs(col)
+        # For the cities in a current row, if they weren't visited, do DFS on the 
+        for city in range(cities_in_row):
+            if not visited[city]:
+                dfs(city)
+                total_provinces += 1
+        return total_provinces
+        
+#         def mark_province_visited(graph, starting_city, visited):
+#             stack = []
+#             stack.append(starting_city)
+            
+#             while len(stack) > 0:
+#                 starting_city = stack.pop()
+                
+#                 neighbors = graph[starting_city]
+                
+#         for city in range(cities_in_row):
+#             if city in visited: continue
+            
+#             total_provinces += 1
+#             mark_province_visited(isConnected, city, visited)
+            
+
+#         # Rows
+#         for i in range(rows):
+#             # Cols
+#             for j in range(cols):
+#                 if isConnected[i][j] == 1:
+#                     dfs(isConnected, i, j)
+#                     num_pr += 1
+
+#         return num_pr
+        
+        
+        
+# Initial Approach
+# Info ;
+# PROVINCES CAN BE BOTH Connected OR Unconnected
+# As you traverse the graph, flag the visited provinces.
+
+# [        0 1 2
+#       _________
+#     0 | [1,1,0],  
+#     1 | [1,1,0], [0]->[1]
+#     2 | [0,0,1]   [2]
+#       Num Provinces = 2
+# ]
+
+# [
+#     [1,0,0],
+#     [0,1,0],
+#     [0,0,1]
+# ]

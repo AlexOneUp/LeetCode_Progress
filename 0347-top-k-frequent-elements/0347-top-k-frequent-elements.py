31bc2601e652253c856b8b565cc97f25c28d12ff
Alex <ruanalex11235@gmail.com>
2024-09-29 00:08:46 -0400

    Time: 1420 ms (5.01%), Space: 22 MB (25.70%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
                 
-        freq = {x: nums.count(x) for x in set(nums)}
-        sorted_freq = dict(sorted(freq.items(), key=lambda item: item[1], reverse=True))
-        res=[]
-        top_k_counter = k
-        for key, val in sorted_freq.items():
-                if top_k_counter == 0 :
-                    break
-                else:
-                    res.append(key)
-                
-                top_k_counter-=1
- 
+        count = {x: nums.count(x) for x in set(nums)}
+        freq = [[] for n in range(len(nums) + 1)]
+
+        # Bucket sorting to store the occurences
+        for num, c in count.items():
+            freq[c].append(num)
+        
+        # print(freq)
+        top_k = k
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                if top_k != 0:
+                    res.append(n)
+                    top_k -= 1 
         return res
+

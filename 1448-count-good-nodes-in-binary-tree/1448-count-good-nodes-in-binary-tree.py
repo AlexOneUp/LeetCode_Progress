4d15372ff48917a2a58d1dab8a160910572c8d0a
Alex <ruanalex11235@gmail.com>
2024-10-13 21:37:00 -0400

    Time: 127 ms (71.86%), Space: 31.2 MB (15.79%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -4,13 +4,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
+
         if not root:
             return 0
-        
         count = 0
-        
+        tmp = root.val
+
         def dfs(cur, max_val):
             nonlocal count
             if not cur:
@@ -18,11 +26,31 @@ class Solution:
             if cur.val >= max_val:
                 count += 1
                 max_val = cur.val
+
             dfs(cur.left, max_val)
             dfs(cur.right, max_val)
-        
-        dfs(root,root.val)
+
+        dfs(root, tmp)
         return count
+
         
-# Approach
-# DFS from left to right returning all nodes that are greater or equal to the root node
+
+
+'''
+Good nodes : nodes less than the root node + path from root has no nodes > x
+
+constraints : 
+    - 1 -> 100 nodes
+    - node.vals can be -100 -> 100
+
+approach:
+    we can perform DFS on it
+
+    DFS :
+        - keep running max of the node its currently at
+        - if the nodes lower than the root val,
+            -   dfs and pass in root, tmp val
+
+
+
+'''

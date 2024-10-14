4c2eac6681fac653b93e8ec3baa3f57898200133
Alex <ruanalex11235@gmail.com>
2024-10-13 20:54:37 -0400

    Time: 174 ms (74.76%), Space: 19.8 MB (57.18%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,42 +1,50 @@
 class MyHashMap:
 
     def __init__(self):
-        # Initialize a list with 1000 empty buckets
-        self.size = 1000
+        #Limit is 1000 size and each bucket inits with None
+        self.size = 10**3
         self.buckets = [[] for _ in range(self.size)]
+        
     
-    def hash(self, key):
-        # Hash function to determine the bucket index
+    def hash_func(self, key):
+        # The hash function is usually calculated by 
+        # returns the hash function
+        # Hash Function : key % size
         return key % self.size
     
+    # void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
     def put(self, key: int, value: int) -> None:
-        # Insert key-value pair into HashMap
-        hash_key = self.hash(key)
-        # Check if the key already exists, if so, update the value
-        for i, (k, v) in enumerate(self.buckets[hash_key]):
-            if k == key:
-                self.buckets[hash_key][i] = (key, value)
-                return
-        # If key doesn't exist, append the new key-value pair
-        self.buckets[hash_key].append((key, value))
+        #compute hash index
+        hash_idx = self.hash_func(key)
 
+        # check if key, val pairs present in bucket
+        for pair in self.buckets[hash_idx]:
+            if pair[0] == key:
+                # print(pair)
+                pair[1] = value
+                return
+            
+        #if key isnt found, append the (k, v) pair
+        self.buckets[hash_idx].append([key, value])
+        
+
+    # int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
     def get(self, key: int) -> int:
-        # Return the value for the given key, or -1 if it doesn't exist
-        hash_key = self.hash(key)
-        for k, v in self.buckets[hash_key]:
-            if k == key:
-                return v
+        hash_idx = self.hash_func(key)
+        for pair in self.buckets[hash_idx]:
+                if key == pair[0]:
+                    return pair[1]
         return -1
 
-    def remove(self, key: int) -> None:
-        # Remove the key-value pair if it exists
-        hash_key = self.hash(key)
-        for i, (k, v) in enumerate(self.buckets[hash_key]):
-            if k == key:
-                del self.buckets[hash_key][i]
-                return
-      
+    # void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
 
+    def remove(self, key: int) -> None:
+        hash_idx = self.hash_func(key)
+        for pair in self.buckets[hash_idx]:
+                if key == pair[0]:
+                    self.buckets[hash_idx].remove(pair)
+                    return
+        
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()

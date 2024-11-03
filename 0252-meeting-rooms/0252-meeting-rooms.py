86c925e9039b9c8a3f7d36708a74a825eed1265d
Alex <ruanalex11235@gmail.com>
2024-11-02 21:15:14 -0400

    Time: 5 ms (15.10%), Space: 19.1 MB (77.79%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -4,8 +4,9 @@ class Solution:
         print(intervals)
         
         for i in range(len(intervals) - 1):
-            if intervals[i][1] > intervals[i+1][0]:
-                return False
+            if intervals[i][1] <= intervals[i+1][0]:
+                continue
+            else: return False
         return True
         
 '''

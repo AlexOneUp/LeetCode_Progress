5b0e0ace97c5d3fc1aa7c21e77782fb2be326e4e
Alex <ruanalex11235@gmail.com>
2024-11-02 21:14:11 -0400

    Time: 7 ms (8.69%), Space: 19.2 MB (77.31%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,26 +1,21 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort(key=lambda x:x[0])
+        print(intervals)
         
-        prev = float('-inf')
-    # Step 2: Iterate through the intervals and check for overlap.
         for i in range(len(intervals) - 1):
-            # Compare the end time of the current interval with the start time of the next interval.
-            if intervals[i][1] > intervals[i + 1][0]:
-                return False  # Found an overlap, so return False.
-        
+            if intervals[i][1] > intervals[i+1][0]:
+                return False
         return True
+        
 '''
-Meeting 
-if the meetings overlap, then return false
+Intuition :
+    [a, b], [c, d]
+    if interval :
+        if they overlap, compare c and b
+            if c <= b, then they overlap
+        usually is if d < b
+        not an interval
+    
 
-otherwise continue return true
-
-
-Intuition
-We have a list of intervals
-A person should be able to attend all meetings
-A person can only be at one meeting at a time
-So there must not be any overlaps
-We need to check whether or not if we find any overlaps
 '''

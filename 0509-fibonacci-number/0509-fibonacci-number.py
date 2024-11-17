79963518a084e48b87ba252502c21988e85ac6dd
Alex <ruanalex11235@gmail.com>
2024-11-17 18:47:43 -0500

    Time: 38 ms (50.98%), Space: 16.6 MB (38.34%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,28 +1,23 @@
 class Solution:
     def fib(self, n: int) -> int:
-        memo = {0:0, 1:1}
+        if n <= 1: return n
         
-        def f(x):
-            if x in memo:
-                return memo[x]
-            else:
-                memo[x] = f(x-1) + f(x-2)
-                return memo[x]
-        return f(n)
+        dp = [0] * (n + 1)
+        
+        dp[1] = 1
+        
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 2] + dp[i - 1]
+        return dp[n]
+        
 '''
-MEMOIZATION :
-{
 
-}
-
-                    F(6) decision tree
-            F(5)           F(4)
-        F(4)  F(3)       F(3) F(2)
-      F(3) F(2) 
-Intuition :
-    - Top-down memoization approach
-    
-    
-
-
+Intuition:
+    Using Bottom-Up Tab approach:
+        Iterative solution
+        
+        An iterative approach 
+            - solve all subproblems first
+                + use their results to build up to the solution of the main problem.
+        
 '''

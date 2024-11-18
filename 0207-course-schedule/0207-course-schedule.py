9c0bf2d42b71007eedd2372ea1b0760912d1dd76
Alex <ruanalex11235@gmail.com>
2024-11-17 21:30:24 -0500

    Time: 7 ms (48.30%), Space: 18.6 MB (30.57%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,50 +1,26 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj_list = {node: [] for node in range(numCourses)}  # Initialize each node with an empty list
-
-        for node in prerequisites:
-            course, prerequisite = node
-            adj_list[course].append(prerequisite) 
-
-        visited = set()
         
+        adj_list = {edge: [] for edge in range(numCourses)}
+        
+        for u, v in prerequisites:
+            adj_list[u].append(v)
+        visited = set()
+            
         def dfs(course):
- 
-            if course in visited:
-                return False
+            if course in visited: return False # cycle found
             
-            #If the course adj_list is empty
-            # Then, we know that course can be completed
-            if adj_list[course] == []:
-                return True
-            visited.add(course)
+            if adj_list[course] == []: return True #init path
             
-            for pre_req in adj_list[course]:
-                # If the course cannot be completed 
-                # If the course has been visited before
+            visited.add(course) #visiting current path
+            
+            for pre_req in adj_list[course]: # fo
                 if not dfs(pre_req): return False
             
-            # Remove from visted set if a course can be completed
             visited.remove(course)
             adj_list[course] = []
-            
             return True
         
-        
-        for course in range(numCourses):
-                if not dfs(course): return False
+        for crs in range(numCourses):
+            if not dfs(crs): return False
         return True
-        
-        
-'''
-is it possible to finish all courses
-
-0 - > [1]
-
-build out adjacency list
-hashmap
-basecase:
-    when a node does not have a pre-requisite
-
-
-'''

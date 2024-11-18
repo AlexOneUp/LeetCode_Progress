5d3b26de614b5e25f240474ad7f33d5910be63be
Alex <ruanalex11235@gmail.com>
2024-11-17 19:21:02 -0500

    Time: 0 ms (100.00%), Space: 16.7 MB (22.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,17 +1,42 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
         memo = {
-            1: 1,
-            2: 2
+            1:1, 
+            2:2
         }
-        def backtrack(n):
-            if n in memo:
-                # Takes care of the base cases of when num is 1 or 2 because a HMap lookup is very efficient
-                # Takes care of time excession
-                return memo[n]
-            
-            memo[n] = backtrack(n - 1) + backtrack(n - 2)
-            return memo[n]
+        
+        def dp(i):
+            if i in memo: return memo[i]
+            memo[i] = dp(i-1) + dp(i-2)
+            return memo[i]
+        dp(n)
+        return memo[n]
+    
+    
+'''
+N steps to reach top
+    -Can climb 
+        1 steps
+        2 steps
 
-        return backtrack(n)
+UMPIRE
+
+U :
+    - There are repeated sub problems
+        n = 2
+        [1,1], [2] = variations
+        
+        n = 5
+        [1,1,1,1,1],[1,1,1,2],[2,2,1],[1,2,2], ..., 
+        
+
+        
+M :
+
+    Either backtrack or DP to solve
+        - dp top down - memo
+P :
+    1. Memoize the initial 2 steps for base case
+        
+
+'''
